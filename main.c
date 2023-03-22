c5892145a4e72dabe50801ed59faac13008f8fbf
kayikci <damla.kayikci@boun.edu.tr>
2023-03-22 17:29:26 +0300

    ufak denemeler

--- main.c
+++ main.c
@@ -22,13 +22,14 @@ typedef enum {
     TOKEN_TYPE_COMMA,
     TOKEN_TYPE_IDENTIFIER,
     TOKEN_TYPE_EQUALS,
-}   TokenType;
-
+} TokenType;
 
 typedef struct {
     TokenType type;
     int value;
-}   Token;
+} Token;
+
+
 
 int main() {
     char input[MAX_LENGTH];
@@ -39,8 +40,75 @@ int main() {
     while (!controller) {
         fgets(input, MAX_LENGTH, stdin);
         int i = 0;
-        str[i] = strtok(input, "");
+        str = strtok(input, "");
+        char var_name[MAX_LENGTH] = ""; // Initialize variable name to empty string
+        int var_name_len = 0; // Initialize variable name length to 0
+        int state = 0; // Initialize state to 0
+        while (str != NULL) {
+            switch (state) {
+                case 0: // Initial state
+                    if (isalpha(str[i])) {
+                        var_name[var_name_len++] = str[i];
+                        state = 1;
+                    } else if (isdigit(str[i])) {
+                        state = 2;
+                    } else if (str[i] == '+') {
+                        state = 3;
+                    } else if (str[i] == '-') {
+                        state = 4;
+                    } else if (str[i] == '*') {
+                        state = 5;
+                    } else if (str[i] == '/') {
+                        state = 6;
+                    } else if (str[i] == '&') {
+                        state = 7;
+                    } else if (str[i] == '|') {
+                        state = 8;
+                    } else if (str[i] == '^') {
+                        state = 9;
+                    } else if (str[i] == '<') {
+                        state = 10;
+                    } else if (str[i] == '>') {
+                        state = 11;
+                    } else if (str[i] == '!') {
+                        state = 12;
+                    } else if (str[i] == '(') {
+                        state = 13;
+                    } else if (str[i] == ')') {
+                        state = 14;
+                    } else if (str[i] == ',') {
+                        state = 15;
+                    } else if (str[i] == '=') {
+                        state = 16;
+                    } else if (str[i] == ' ') {
+                        state = 0;
+                    } else {
+                        state = 17;
+                    }
+                    break;
+                case 1: // Variable name
+                    if (isalpha(str[i])) {
+                        var_name[var_name_len++] = str[i];
+                        state = 1;
+                    } else if (isdigit(str[i])) {
+                        var_name[var_name_len++] = str[i];
+                        state = 1;
+                    } else if (str[i] == '+') {
+                        state = 3;
+                    } else if (str[i] == '-') {
+                        state = 4;
+                    } else if (str[i] == '*') {
+                        state = 5;
+                    } else if (str[i] == '/') {
+                        state = 6;
+                    } else if (str[i] == '&') {
+                        state = 7;
+                    } else if (str[i]
+
+
 
     }
 
-}
+
+
+
