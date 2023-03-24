abbcd7a9394ff4aaec848bdb2ae9bd45ff9a4dd2
Cgty <107434789+CagatayColak@users.noreply.github.com>
2023-03-25 01:43:13 +0300

    25mart

---
+++ save.cpp
@@ -0,0 +1,210 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+#define MAX_LENGTH 256
+
+typedef enum {
+    TOKEN_TYPE_NUMBER,
+    TOKEN_TYPE_PLUS,
+    TOKEN_TYPE_MINUS,
+    TOKEN_TYPE_ASTERISK,
+    TOKEN_TYPE_AMPERSAND,
+    TOKEN_TYPE_PIPE,
+    TOKEN_TYPE_XOR,
+    TOKEN_TYPE_LS,
+    TOKEN_TYPE_RS,
+    TOKEN_TYPE_LR,
+    TOKEN_TYPE_RR,
+    TOKEN_TYPE_NOT,
+    TOKEN_TYPE_OPENPARENTHESIS,
+    TOKEN_TYPE_CLOSEPARENTHESIS,
+    TOKEN_TYPE_COMMA,
+    TOKEN_TYPE_IDENTIFIER,
+    TOKEN_TYPE_EQUALS,
+} TokenType;
+
+typedef struct {
+    TokenType type;
+    char *name;
+    char *value;
+} Token;
+
+Token tokenizer(char input, int variable_count, Token variables);
+
+int main() {
+    char input[MAX_LENGTH];
+    int controller = 0;
+    Token *variables[MAX_LENGTH];
+    int variable_count = 0;
+    while (!controller) { //general while loop
+        fgets(input, MAX_LENGTH, stdin); // taking input
+        Token *infix[MAX_LENGTH];
+        strcpy(infix,tokenizer(input, variable_count, variables)); //tokenize the input
+    }
+}
+int isOperator(char ch){
+    return (ch == '^' || ch == '<' || ch == '>' || ch == '$' || ch == '€');
+}
+
+char formatController(Token input){
+    int i = 0;
+    char *flag = malloc(sizeof(char) * sizeof (input));
+    int flag_count = 0;
+    char *flag2 = malloc(sizeof(char) * sizeof (input));
+    int flag_count2 = 0;
+    while(i < sizeof(input)){
+        if(isOperator(input[i]->name) && i != strlen(input) - 1){
+            int j = i;
+            while(j < sizeof(input)){
+                if (input[i+1]->name == "("){
+
+                }
+                else{
+                    printf("Error: Invalid syntax");
+                    return 0;
+                }
+                j++;
+            }
+        }
+        else if(input[i]->name == "!" && i != strlen(input) - 1){
+
+        }
+        i++;
+    }
+}
+
+Token tokenizer(char *input, int variable_count, Token variables) { //tokenizer function
+    Token *token[MAX_LENGTH];
+    int i = 0;
+    int token_index = 0;
+    char *str;
+    int strlength = 0;
+    int input_length = strlen(input);
+    for (i = 0; i < MAX_LENGTH; i++) {
+        if (input[i] == '\0') {
+            break;
+        } else if (input[i] == ' ') {
+            continue;
+        } else if (isalpha(input[i])) {
+            str[strlength] = input[i];
+            strlength++;
+            if (isalpha(input[i + 1]) && (input_length != (i + 1))) {
+                continue;
+            } else {
+                if (strcmp(str, "xor") == 0) {
+                    token[token_index]->type = TOKEN_TYPE_XOR;
+                    token[token_index]->name = "^";
+                } else if (strcmp(str, "not") == 0) {
+                    token[token_index]->type = TOKEN_TYPE_NOT;
+                    token[token_index]->name = "!";
+                    if (input[i + 1] == '(') {
+                    }else{
+                        printf("Error!");
+                    }
+                } else if (strcmp(str, "ls") == 0) {
+                    token[token_index]->type = TOKEN_TYPE_LS;
+                    token[token_index]->name = "<";
+                    if (input[i + 1] == '(') {
+                    }else{
+                        printf("Error!");
+                    }
+                } else if (strcmp(str, "rs") == 0) {
+                    token[token_index]->type = TOKEN_TYPE_RS;
+                    token[token_index]->name = ">";
+                    if (input[i + 1] == '(') {
+                    }else{
+                        printf("Error!");
+                    }
+                } else if (strcmp(str, "lr") == 0) {
+                    token[token_index]->type = TOKEN_TYPE_LR;
+                    token[token_index]->name = "$";
+                    if (input[i + 1] == '(') {
+                    }else{
+                        printf("Error!");
+                    }
+                } else if (strcmp(str, "rr") == 0) {
+                    token[token_index]->type = TOKEN_TYPE_RR;
+                    token[token_index]->name = "€";
+                    if (input[i + 1] == '(') {
+                    }else{
+                        printf("Error!");
+                    }
+                } else {
+                    token[token_index]->type = TOKEN_TYPE_IDENTIFIER;
+                    token[token_index]->name = str;
+                    variables[variable_count]->name = str;
+                    variables[variable_count]->value = 0;
+                    variables[variable_count]->type = TOKEN_TYPE_IDENTIFIER;
+                    variable_count++;
+                }
+                token_index++;
+                str = NULL;
+                strlength = 0;
+            }
+        } else if (isdigit(input[i])) {
+            str[strlength] = input[i];
+            strlength++;
+            if (isdigit(input[i + 1]) && (input_length != (i + 1))) {
+                continue;
+            } else {
+                token[token_index]->type = TOKEN_TYPE_NUMBER;
+                token[token_index]->name = str;
+                token[token_index]->value = str;
+                token_index++;
+                str = NULL;
+                strlength = 0;
+            }
+        } else if (input[i] == '+') {
+            token[token_index]->type = TOKEN_TYPE_PLUS;
+            token[token_index]->name = "+";
+            token_index++;
+        } else if (input[i] == '-') {
+            token[token_index]->type = TOKEN_TYPE_MINUS;
+            token[token_index]->name = "-";
+            token_index++;
+        } else if (input[i] == '*') {
+            token[token_index]->type = TOKEN_TYPE_ASTERISK;
+            token[token_index]->name = "*";
+            token_index++;
+        } else if (input[i] == '|') {
+            token[token_index]->type = TOKEN_TYPE_PIPE;
+            token[token_index]->name = "|";
+            token_index++;
+        } else if (input[i] == '&') {
+            token[token_index]->type = TOKEN_TYPE_AMPERSAND;
+            token[token_index]->name = "&";
+            token_index++;
+        } else if (input[i] == '(') {
+            token[token_index]->type = TOKEN_TYPE_OPENPARENTHESIS;
+            token[token_index]->name = "(";
+            token_index++;
+        } else if (input[i] == ')') {
+            token[token_index]->type = TOKEN_TYPE_CLOSEPARENTHESIS;
+            token[token_index]->name = ")";
+            token_index++;
+        } else if (input[i] == '=') {
+            token[token_index]->type = TOKEN_TYPE_EQUALS;
+            token[token_index]->name = "=";
+            token_index++;
+            int k = 0;
+            char *rightsideoftheequals[MAX_LENGTH - i];
+            for (int k = 0; k < MAX_LENGTH - i; k++) {
+                rightsideoftheequals[k] = input[i + k];
+            }
+            int result = calculator(infixtopostfix(tokenizer(rightsideoftheequals, 0, variables))); //result of the right expression
+            if (strcmp(str, variables[variable_count - 1]->name) == 0) { //check if the variable is same as the last one
+                variables[variable_count - 1]->value = result;
+            } else { //if not, give an error
+                printf("Error!");
+            }
+        }
+    }
+    return token;
+}
+
+
+
+
+
